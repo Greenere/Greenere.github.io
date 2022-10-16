e78dd8e25e2e6af40dab9feb99e86d7b9849692c
Greenere <2897643352@qq.com>
2022-10-16 01:00:18 -0400

    add carbon city

---
+++ visualizations/lllenroc_carbon_city/carbon_chart.js
@@ -0,0 +1,139 @@
+const svg_carbon = d3.select("svg#carbon_chart");
+const carbon_margin = { left: 100, right: 100, top: 50, bottom: 50 };
+const carbon_width =
+  svg_carbon.attr("width") - carbon_margin.left - carbon_margin.right;
+const carbon_height =
+  svg_carbon.attr("height") - carbon_margin.top - carbon_margin.bottom;
+const main_color = '#ff006e';
+
+const carbon_chart_labels = svg_carbon
+.append("g")
+.attr("id", "carbon_label_group")
+.attr("transform", `translate(${carbon_margin.left}, ${carbon_margin.top})`);
+
+const carbon_chart = svg_carbon
+  .append("g")
+  .attr("id", "carbon_group")
+  .attr("transform", `translate(${carbon_margin.left}, ${carbon_margin.top})`);
+
+const iteration_scale = d3.scaleLinear([0, 19], [0, carbon_width]);
+let bars = new Array();
+for (i = 0; i < stat_size; i++){
+  bars.push(i);
+}
+
+carbon_chart_labels.selectAll('line.iteration_label')
+.data(bars)
+.join('line')
+.attr('class', 'iteration_label')
+.attr('x1', d=>iteration_scale(d))
+.attr('y1', 0)
+.attr('x2', d=>iteration_scale(d))
+.attr('y2', carbon_height)
+.attr('stroke', 'slategray')
+
+carbon_chart.append("text") 
+.attr("x", 850)
+.attr("y", 430)
+.attr("text-anchor","middle")
+.text("Iteration")
+.attr('fill', 'slategray');
+carbon_chart.append("text") 
+.attr("x", -30)
+.attr("y", 180)
+.attr("transform",`rotate(-90, -30 ${carbon_height / 2})`)
+.attr("text-anchor","middle")
+.text("Estimated Carbon Emissions per Capita")
+.attr('fill', 'slategray')
+
+function get_emission_data(carbon_stats){
+  var carbon_emission_data = new Array();
+  for (i = 0; i < carbon_stats.length; i++) {
+    carbon_stats[i].forEach((d) => {
+      carbon_emission_data.push({
+        iteration: i,
+        emission: d,
+      });
+    });
+  }
+  return carbon_emission_data;
+}
+
+function update_carbon_chart() {
+  carbon_emission_data = get_emission_data(carbon_stats);
+
+  carbon_range = d3.extent(carbon_emission_data.map((d) => d.emission));
+  emission_scale = d3.scaleLinear(carbon_range, [carbon_height, 0]);
+
+  carbon_chart
+    .selectAll("circle.carbon_dots")
+    .data(carbon_emission_data)
+    .join((enter) => {
+      enter
+        .append("circle")
+        .attr("class", "carbon_dots")
+        .attr("cx", (d) => iteration_scale(d.iteration))
+        .attr("cy", (d) => emission_scale(d.emission))
+        .attr("r", 5)
+        .attr("opaicty", 0.8)
+        .attr("fill", main_color);
+    },
+    (update)=>{
+      update.attr("cx", (d) => iteration_scale(d.iteration))
+      .attr("cy", (d) => emission_scale(d.emission))
+    });
+}
+
+function update_carbon_chart_histogram() {
+  carbon_emission_data = get_emission_data(carbon_stats);
+
+  carbon_range = d3.extent(carbon_emission_data.map((d) => d.emission));
+  emission_scale = d3.scaleLinear(carbon_range, [carbon_height, 0]);
+  histo = d3.histogram()
+  .value(d=>d)
+  .domain(carbon_range)
+  .thresholds(histo_bin_num)
+  for (i = 0; i < carbon_stats.length; i++){
+    data = carbon_stats[i]
+
+    bins = histo(data)
+    size_scale = d3.scaleLinear(d3.extent(bins, (d)=>d.length),[0,10])
+
+  carbon_chart
+    .selectAll(`circle.carbon_dot_${i}`)
+    .data(bins)
+    .join((enter) => {
+      enter
+        .append("circle")
+        .attr("class", `carbon_dot_${i}`)
+        .attr("cx", iteration_scale(i))
+        .attr("cy", (d) => emission_scale(d.x0))
+        .attr("r", (d)=>size_scale(d.length))
+        .attr("opaicty", 0.5)
+        .attr("fill", main_color);
+    },
+    (update)=>{
+      update.attr("cx", iteration_scale(i))
+      .attr("cy", (d) => emission_scale(d.x0))
+      .attr("r", (d)=>size_scale(d.length))
+    });
+  }
+  if (carbon_stats.length > 0){
+  carbon_chart_labels.selectAll("text.indicator")
+  .data([carbon_stats.length-1])
+  .join((enter)=>{
+    enter.append('text')
+    .attr('class','indicator')
+    .attr('x', d=>iteration_scale(d))
+    .attr('y', -10)
+    .attr('fill', main_color)
+    .attr('text-anchor','middle')
+    .text('current')
+  },
+  (update)=>{
+    update.attr('x', d=>iteration_scale(d))
+  })
+  }
+}
+
+update_carbon_chart_histogram();
---
+++ visualizations/lllenroc_carbon_city/city_blocks.js
@@ -0,0 +1,284 @@
+const random_walk_num = 100;
+const histo_bin_num = 20;
+
+var target_residence = 0;
+
+var current_best_blocks = null;
+var current_best_carbon = Infinity;
+
+// Generate basic blocks
+const block_num = 10;
+function get_coordinates(index) {
+  return {
+    x: index % block_num,
+    y: (index - (index % block_num)) / block_num,
+  };
+}
+
+var blocks = new Array();
+for (i = 0; i < block_num * block_num; i++) {
+  coords = get_coordinates(i);
+  blocks.push({
+    value: 0,
+    x: coords.x,
+    y: coords.y,
+  });
+}
+
+function get_index(x, y) {
+  return x + y * block_num;
+}
+
+var current_placement = 0;
+
+function changePlacement0() {
+  current_placement = 0;
+  document.getElementById("placement_value").innerHTML = "Remove";
+  // change icon buttons active status
+  document.getElementById("residence_button").classList.remove("active");
+  document.getElementById("commercial_button").classList.remove("active");
+  document.getElementById("park_button").classList.remove("active");
+  document.getElementById("remove_button").classList.add("active");
+}
+
+function changePlacement1() {
+  current_placement = 1;
+  document.getElementById("placement_value").innerHTML = "Residence";
+  // change icon buttons active status
+  document.getElementById("residence_button").classList.add("active");
+  document.getElementById("commercial_button").classList.remove("active");
+  document.getElementById("park_button").classList.remove("active");
+  document.getElementById("remove_button").classList.remove("active");
+}
+function changePlacement2() {
+  current_placement = 2;
+  document.getElementById("placement_value").innerHTML = "Commercial";
+  // change icon buttons active status
+  document.getElementById("residence_button").classList.remove("active");
+  document.getElementById("commercial_button").classList.add("active");
+  document.getElementById("park_button").classList.remove("active");
+  document.getElementById("remove_button").classList.remove("active");
+}
+function changePlacement3() {
+  current_placement = 3;
+  document.getElementById("placement_value").innerHTML = "Park";
+  // change icon buttons active status
+  document.getElementById("residence_button").classList.remove("active");
+  document.getElementById("commercial_button").classList.remove("active");
+  document.getElementById("park_button").classList.add("active");
+  document.getElementById("remove_button").classList.remove("active");
+}
+function remove() {
+  document.getElementById("placement_value").innerHTML = "";
+  // change icon buttons active status
+  document.getElementById("residence_button").classList.remove("active");
+  document.getElementById("commercial_button").classList.remove("active");
+  document.getElementById("park_button").classList.remove("active");
+  document.getElementById("remove_button").classList.add("active");
+}
+function random() {
+  document.getElementById("placement_value").innerHTML = "";
+  // remove icon buttons active status
+  document.getElementById("residence_button").classList.remove("active");
+  document.getElementById("commercial_button").classList.remove("active");
+  document.getElementById("park_button").classList.remove("active");
+  document.getElementById("remove_button").classList.remove("active");
+
+  cur_residence = 0;
+  blocks.forEach((d) => {
+    if (cur_residence < target_residence) {
+      nex = Math.round(Math.random() * 3);
+    } else {
+      nex = 2 + Math.round(Math.random() * 4);
+      if (nex == 4) {
+        nex = 0;
+      }
+    }
+    if (nex == 1) {
+      cur_residence += 1;
+    }
+    d.value = nex;
+  });
+  update_blocks();
+  update_card_texts();
+  update_counts_and_carbon();
+  update_carbon_chart_histogram();
+}
+
+function reset() {
+  document.getElementById("placement_value").innerHTML = "";
+  // remove icon buttons active status
+  document.getElementById("residence_button").classList.remove("active");
+  document.getElementById("commercial_button").classList.remove("active");
+  document.getElementById("park_button").classList.remove("active");
+  document.getElementById("remove_button").classList.remove("active");
+  blocks.forEach((d) => {
+    d.value = 0;
+  });
+  update_blocks();
+  update_card_texts();
+  update_counts_and_carbon();
+  update_carbon_chart_histogram();
+}
+
+function loadBest() {
+  document.getElementById("placement_value").innerHTML = "";
+  document.getElementById("residence_button").classList.remove("active");
+  document.getElementById("commercial_button").classList.remove("active");
+  document.getElementById("park_button").classList.remove("active");
+  document.getElementById("remove_button").classList.remove("active");
+
+  if (current_best_blocks != null) {
+    blocks = JSON.parse(JSON.stringify(current_best_blocks));
+  }
+  update_blocks();
+  update_card_texts();
+  update_counts_and_carbon();
+  update_carbon_chart_histogram();
+}
+
+
+
+function setTarget() {
+  
+  target_residence = prompt("Set the target residence", "");
+  if (target_residence > 50 || target_residence < 1) {
+    alert("Set the target between 1 and 50");
+  }
+
+  update_card_texts();
+
+}
+
+
+var block_counts = new Array();
+var carbon_stats = new Array();
+const stat_size = 20;
+
+const svg_city = d3.select("svg#city");
+const city_margin = { left: 50, right: 50, top: 50, bottom: 50 };
+const city_width =
+  svg_city.attr("width") - city_margin.left - city_margin.right;
+const city_height =
+  svg_city.attr("height") - city_margin.top - city_margin.bottom;
+
+const block_size = (city_width / block_num) * 0.85;
+const block_gap = (city_width / block_num) * 0.15;
+
+const block_group = svg_city
+  .append("g")
+  .attr("id", "block_group")
+  .attr("transform", `translate(${city_margin.left}, ${city_margin.top})`);
+
+const color_scale = (x) => {
+  if (x == 1) {
+    // residence
+    return "#fcf6bd";
+  } else if (x == 2) {
+    return "#4cc9f0"; // commercial
+  } else if (x == 3) {
+    return "#94bfa7"; // green
+  } else if (x == 4) {
+    return "gray";
+  }
+  return "white";
+};
+
+function update_counts_and_carbon() {
+  block_counts.push(get_statistics(blocks));
+  carbon_stats.push(get_carbons(blocks, random_walk_num));
+  if (block_counts.length > stat_size) {
+    block_counts.shift();
+  }
+  if (carbon_stats.length > stat_size) {
+    carbon_stats.shift();
+  }
+}
+
+function update_card_texts() {
+  cur = get_statistics(blocks);
+  if (carbon_stats.length > 0) {
+    cur_stat = carbon_stats[carbon_stats.length - 1];
+  } else {
+    cur_stat = get_carbons(blocks, random_walk_num);
+  }
+  const average = (array) => array.reduce((a, b) => a + b) / array.length;
+  const cur_carbon = average(cur_stat);
+  const unemployment =
+    cur.commercial > cur.residence ? 0 : cur.residence - cur.commercial;
+  document.getElementById("res_block_stat").innerText =
+    "Residence:" + cur.residence;
+  document.getElementById("com_block_stat").innerText =
+    "Commercial:" + cur.commercial;
+  document.getElementById("park_block_stat").innerText = "Park:" + cur.green;
+  document.getElementById("carbon_per_stat").innerText =
+    "Avg Carbon:\n" + cur_carbon;
+  document.getElementById("unemployment_stat").innerText =
+    "Unemployment:\n" + unemployment;
+  document.getElementById("target_residence").innerText =
+    "Target Residence:\n" + target_residence;
+  document.getElementById("best_carbon").innerText =
+    "Best Carbon:\n" + current_best_carbon;
+
+  if (unemployment < 1 && cur.residence > target_residence - 1) {
+    if (cur_carbon < current_best_carbon && cur_carbon > 0) {
+      current_best_carbon = cur_carbon;
+      current_best_blocks = JSON.parse(JSON.stringify(blocks));
+    }
+  }
+}
+
+function update_blocks() {
+  block_group
+    .selectAll("rect.block")
+    .data(blocks)
+    .join(
+      (enter) => {
+        enter
+          .append("rect")
+          .attr("class", "block")
+          .attr("x", (d) => (block_size + block_gap) * d.x)
+          .attr("y", (d) => (block_size + block_gap) * d.y)
+          .attr("width", block_size)
+          .attr("height", block_size)
+          .attr("stroke", "lightgrey")
+          .attr("fill", (d) => color_scale(d.value))
+          .attr("rx", block_gap)
+          .attr("stroke-width", 2)
+          .on("mouseover", function (event) {
+            let target = d3.select(this);
+            target
+              .attr("stroke-width", 4)
+              .attr("stroke", "black")
+              .attr("fill", color_scale(current_placement));
+          })
+          .on("mouseout", function (event) {
+            let target = d3.select(this);
+            target
+              .attr("stroke-width", 2)
+              .attr("stroke", "lightgrey")
+              .attr("fill", (d) => color_scale(d.value));
+          })
+          .on("mousedown", function (event) {
+            let target = d3.select(this);
+            let index = get_index(target.datum().x, target.datum().y);
+            blocks[index].value = current_placement;
+            update_blocks();
+            update_card_texts();
+            update_counts_and_carbon();
+            update_carbon_chart_histogram();
+          });
+      },
+      (update) => {
+        update.attr("fill", (d) => color_scale(d.value));
+      }
+    );
+}
+
+update_blocks();
+update_card_texts();
+
+// console.log(get_carbons(blocks, 10));
+// console.log(get_statistics(blocks));
+// console.log(block_counts);
+// console.log(carbon_stats);
